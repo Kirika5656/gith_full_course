708849d4905ff54232b65e8e18033b96d2e53093
Kirika Takenaka <winndy.4190@gmail.com>
2025-07-31 19:55:20 +0900

    関数を呼び出した

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function greet() {
-<<<<<<< HEAD
   console.log("Hello from the main branch!");
-=======
-  console.log("Hello from the local branch!");
->>>>>>> 87f4e91 (私達の修正)
 }
+
+greet();
+
 
 
